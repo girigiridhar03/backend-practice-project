655adec8be9cf6405339e64bc2a77ed673414f08
GIRI GIRIDHAR <girigiridhar0603@gmail.com>
2025-06-15 11:37:57 +0530

    changes

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://frontend-livid-nine-66.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://frontend-livid-nine-66.vercel.app",
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
